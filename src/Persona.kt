2c95ac2d35afab54530ac943e35bb7023c481b5b
rmungar1209 <rmungar1209@g.educaand.es>
2024-01-25 13:26:14 +0100

    comentarios

--- src/Persona.kt
+++ src/Persona.kt
@@ -1,5 +1,8 @@
-class persona(peso: Double, altura: Double){
+/**
+ * La clase persona tiene un constructor primario con el peso y la altura de una persona y cuenta con métodos exclusivos de esta clase.
+ */
 
+class persona(peso: Double, altura: Double){
     var peso: Double = 0.0
         set(value){
             comprobarPeso(value)
@@ -22,20 +25,37 @@ class persona(peso: Double, altura: Double){
         this.nombre = nombre
     }
 
+    /**
+     * La funcion comprobarPeso() se asegura de que el peso que se introduzca para la persona no sea menor o igual que 0.
+     * @param value : Double -> El peso de la persona
+     * @return Retorna el mismo valor en caso de que sea mayor que 0 y en caso de que sea menor, retorna           50.0
+     */
     fun comprobarPeso(value: Double): Double{
         if (value <= 0.0)
-            return 0.0
+            return 50.0
         else{
             return value
         }
     }
+
+    /**
+     * La funcion comprobarAltura() se asegura de que la altura que se introduzca para la persona no sea menor o igual que 0.
+     * @param value : Double -> La altura de la persona
+     * @return Retorna el mismo valor en caso de que sea mayor que 0 y en caso de que sea menor, retorna           1.60
+     */
     fun comprobarAltura(value:Double):Double{
         if (value <= 0.0)
-            return 0.0
+            return 1.60
         else{
             return value
         }
     }
+
+    /**
+     * La funcion cambiarNombre() nos permitirá cambiar el nombre de una persona por otro que ingresará el usuario en la consola.
+     */
+
+
     fun cambiarNombre(){
         var nom = ""
         print("Ingresa el nuevo nombre: ")
@@ -47,6 +67,9 @@ class persona(peso: Double, altura: Double){
         else println("Nombre no modificado")
     }
 
+    /**
+     * cambiarImc() se encarga de cambiar el imc de una persona mediante la modificación de los parámetros peso y altura, que los ingresará el usuario en la consola.
+     */
     fun cambiarImc() {
         var pes = 0.0
         var alt = 0.0
